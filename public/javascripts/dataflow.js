d8a8ae16e58388ae2673ecbf0534a16e8f44a07c
moretgio <moretgio@students.zhaw.ch>
2016-09-10 23:46:06 +0200

    removal of packages works

--- public/javascripts/dataflow.js
+++ public/javascripts/dataflow.js
@@ -5,24 +5,22 @@ $(document).ready(function() {
     var packagesIntervalId;
     var isStarted = false;
 
-
     /************************** FUNCTIONS ****************************/
 
     DataFlow.startDataFlow = function () {
         // TODO: push types
         var i = 0;
-        var types = ["driver", "driver", "man", "public", "employer", "public", "man", "man", "driver", "driver", "man", "public", "employer", "public", "man", "man", "driver", "driver", "man", "public", "employer", "public", "man", "man", "driver", "driver", "man", "public", "employer", "public", "man", "man"];
+        var types = ["driver", "driver", "man", "public", "employer", "public", "man", "man", "driver", "driver", "man", "public", "employer", "public", "man", "man", "driver", "driver", "man", "public", "employer", "public", "man", "man", "driver", "driver", "man", "public", "employer", "public", "man", "man",
+                     "driver", "driver", "man", "public", "employer", "public", "man", "man", "driver", "driver", "man", "public", "employer", "public", "man", "man", "driver", "driver", "man", "public", "employer", "public", "man", "man", "driver", "driver", "man", "public", "employer", "public", "man", "man"];
         if (!isStarted) {
             packagesIntervalId = window.setInterval(function() {
                        if (types.length != 0) {
                            DataFlow.addDataPkg(i, types.shift());
                            i = i + 1;
                        }
-
                    }, 1000);
         }
         isStarted = true;
-
     }
 
      DataFlow.stopDataFlow = function() {
@@ -32,6 +30,27 @@ $(document).ready(function() {
     
     DataFlow.addDataPkg = function(number, type) {
         datapkg = document.createElement('div');
+        cssClass = getAppropriateCssClass(type);
+        $(datapkg).addClass("data-pkg").addClass(type).attr("id", "data-pkg-" + number);
+        setRemovalTimeout(datapkg, number);
+
+        $("#data-flow").append(datapkg);
+        window.setTimeout(function() {
+            console.log("Sending data pkg " + number + " to the " + cssClass);
+            $(datapkg).addClass(cssClass);
+        }, 1000);
+    }
+
+    function setRemovalTimeout(datapkg, number) {
+
+        window.setTimeout(function(){
+            console.log("Removing data pkg " + number);
+            $(datapkg).remove();
+        }, 999);
+
+    }
+
+    function getAppropriateCssClass(type) {
         cssClass = "";
         if (type == "driver") {
             cssClass = "lower-left-corner";
@@ -42,18 +61,7 @@ $(document).ready(function() {
         } else if (type == "public") {
             cssClass = "upper-right-corner";
         }
-        $(datapkg).addClass("data-pkg").addClass(type).attr("id", "data-pkg-" + number);
-        $("#data-flow").append(datapkg);
-        window.setTimeout(function() {
-            console.log("Sending data pkg " + number + " to the " + cssClass);
-            $(datapkg).addClass(cssClass);
-            // TODO: Because I'm a JS noob, the following code is commented out.
-//            window.setTimeout(function() {
-//                console.log("Removing data pkg " + number);
-//                $(datapkg).remove();
-//            }, 3000);
-        }, 1000);
-        
+        return cssClass;
     }
     
 });
